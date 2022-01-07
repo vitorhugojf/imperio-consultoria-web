bcdf366476d319bcf96a4a34487c7fe849e5b6b5
Vitor Hugo <vnascimento@testreach.com>
2022-01-07 19:49:26 -0300

    Adding custom theme

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== 'production'
+  },
+  publicPath: process.env.NODE_ENV === 'production'
     ? '/imperio-consultoria-web/'
     : '/'
 }
