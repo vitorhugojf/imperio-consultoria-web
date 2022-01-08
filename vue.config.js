318727686ae1525f0c42e16fbbb3cd87b4dd6e1b
Vitor Hugo <vnascimento@testreach.com>
2022-01-07 21:41:50 -0300

    Fixing deploy

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   css: {
-    // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/imperio-consultoria-web/'
-    : '/'
+  publicPath: '/imperio-consultoria-web/'
 }
