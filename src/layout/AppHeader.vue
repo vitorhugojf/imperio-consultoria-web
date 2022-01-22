49d4d12e3840ac2158d6e3e51078c742ba6f9137
Vitor Hugo <vnascimento@testreach.com>
2022-01-22 17:13:47 -0300

    Fixing the topbar behavior and adding the dark and light theme with colors

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar app color="transparent" elevation="0">
+  <v-app-bar app :color="color" :elevation="elevation">
     <div class="d-flex align-center">
       <router-link slot="brand" class="navbar-brand mr-lg-5" to="/inicio">
         <v-img
@@ -8,6 +8,7 @@
           contain
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
+          collapse-on-scroll="true"
           width="40"
         />
       </router-link>
@@ -25,13 +26,44 @@
     <v-btn to="/sobre" text>
       <span class="mr-2">Sobre</span>
     </v-btn>
+    <v-btn color="info" small fab @click="toggleDarkTheme">
+      <v-icon v-if="$vuetify.theme.dark" bottom class="mr-1"
+        >mdi-moon-waxing-crescent</v-icon
+      >
+      <v-icon v-else color="yellow">mdi-white-balance-sunny</v-icon>
+    </v-btn>
   </v-app-bar>
 </template>
 <script>
 export default {
   components: {},
   created() {},
-  data: () => ({ logo: "" }),
+  data: () => ({
+    color: "transparent",
+    elevation: 0,
+  }),
+  mounted() {
+    window.onscroll = () => {
+      this.changeColor();
+    };
+  },
+  methods: {
+    changeColor() {
+      if (document.documentElement.scrollTop > 25) {
+        this.color = this.$vuetify.theme.isDark
+          ? this.$vuetify.theme.themes.dark.primary
+          : this.$vuetify.theme.themes.light.primary;
+        this.elevation = 2;
+      } else {
+        this.color = "transparent";
+        this.elevation = 0;
+      }
+    },
+    toggleDarkTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+      this.changeColor();
+    },
+  },
 };
 </script>
 <style></style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,34 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 
+import * as lightColors from "@/assets/scss/custom/colors/_light.scss";
+import * as darkColors from "@/assets/scss/custom/colors/_dark.scss";
+
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: lightColors.primary,
+        secondary: lightColors.secondary,
+        accent: lightColors.accent,
+        error: lightColors.error,
+        info: lightColors.info,
+        success: lightColors.success,
+        warning: lightColors.warning,
+      },
+      dark: {
+        primary: darkColors.primary,
+        secondary: darkColors.secondary,
+        accent: darkColors.accent,
+        error: darkColors.error,
+        info: darkColors.info,
+        success: darkColors.success,
+        warning: darkColors.warning,
+      },
+    },
+  },
+});
+
+export default vuetify;
