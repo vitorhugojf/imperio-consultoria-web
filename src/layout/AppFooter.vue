aa2b10bc6ce54242cfa3880cfd623227fc218f44
Vitor Hugo <vnascimento@testreach.com>
2022-01-22 17:25:34 -0300

    Altering footer

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -3,34 +3,9 @@
     <div class="container">
       <div class="row justify-content-md-between">
         <div class="mb-2 col-md-4">
-          <div class="text-uppercase text-muted font-weight-bold">
-            <b>Nosso Endereço</b>
-          </div>
           <small class="text-uppercase text-muted font-weight-bold"
-            >Rua Assuene Antonio Ribeiro, 803</small
+            >{{year}}</small
           >
-          <br />
-          <small class="text-uppercase text-muted font-weight-bold"
-            >Nova Benfica, Juiz de Fora - 36091050</small
-          >
-        </div>
-        <div class="mb-2 col-md-4">
-          <div class="text-uppercase text-muted font-weight-bold">
-            <b>Horários de atendimento</b>
-          </div>
-          <small class="text-uppercase text-muted font-weight-bold"
-            >Segunda a Sexta - das 8h às 18h</small
-          >
-          <br />
-          <small class="text-uppercase text-muted font-weight-bold"
-            >Sábado - das 8h às 13h</small
-          >
-        </div>
-        <div class="col-md-4">
-          <div class="text-uppercase text-muted font-weight-bold">
-            <b>Nossos telefones</b>
-          </div>
-          <small class="text-uppercase text-muted font-weight-bold"> </small>
         </div>
       </div>
     </div>
