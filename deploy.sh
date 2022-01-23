8a439cc2f512174076fe8a1f1eecbe9dc83d301e
Vitor Hugo <vnascimento@testreach.com>
2022-01-23 11:02:47 -0300

    eiting deploy.sh config

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ yarn run build
 cd dist
 
 git init
+git config --global user.name "Vitor Hugo"
+git config --global user.email "vitorhugojf9@gmail.com"
 git add -A
 git commit -m 'New Deployment'
 git push -f git@github.com:vitorhugojf/imperio-consultoria-web.git master:gh-pages
