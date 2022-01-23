3c492de88809735129bf3ea30067f7fb75ad43a9
Vitor Hugo <vnascimento@testreach.com>
2022-01-23 10:40:25 -0300

    Adding the deploy on push

--- src/views/ByeWorld.vue
+++ src/views/ByeWorld.vue
@@ -11,7 +11,7 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to end</h1>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to end.</h1>
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
